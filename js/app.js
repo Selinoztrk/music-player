4dbfdf502b8f53ee66a0e278fefdd248e8c462bb
Selin Öztürk <ozturkselin066@gmail.com>
2025-07-23 11:31:54 +0300

    feat(player): add manual time control to progress bar

--- js/app.js
+++ js/app.js
@@ -19,69 +19,56 @@ window.addEventListener("load", () => {
     displayMusic(music);
 });
 
-function displayMusic(music) {
+
+const displayMusic = (music) => {
     title.innerText = music.getName();
     singer.innerText = music.singer;
     image.src = "img/" + music.img;
     audio.src = "mp3/" + music.file;
 }
 
+
 play.addEventListener("click", () => {
     const isMusicPlay = container.classList.contains("playing");
     isMusicPlay ? pauseMusic() : playMusic();
 });
 
-prev.addEventListener("click", () => {
-    prevMusic();
-});
 
-next.addEventListener("click", () => {
-    nextMusic();
-});
+prev.addEventListener("click", () => { prevMusic(); });
 
-function prevMusic() {
+
+next.addEventListener("click", () => { nextMusic(); });
+
+
+const prevMusic = () => {
     player.prev();
     let music = player.getMusic();
     displayMusic(music);
     playMusic();
 }
 
-function nextMusic() {
+
+const nextMusic = () => {
     player.next();
     let music = player.getMusic();
     displayMusic(music);
     playMusic();
 }
 
-function pauseMusic() {
+
+const pauseMusic = () => {
     container.classList.remove("playing");
     play.classList = "fa-solid fa-play";
     audio.pause();
 }
 
-function playMusic() {
+
+const playMusic = () => {
     container.classList.add("playing");
     play.classList = "fa-solid fa-pause";
     audio.play();
 }
 
-const calculateTime = (totalSeconds) => {
-    const minute = Math.floor(totalSeconds / 60);
-    const seconds = Math.floor(totalSeconds % 60);
-    const updatedSeconds = seconds < 10 ? `0${seconds}`: `${seconds}`;
-    const result = `${minute}:${updatedSeconds}`;
-    return result;
-}
-
-audio.addEventListener("loadedmetadata", () => {
-    duration.textContent = calculateTime(audio.duration);
-    progressBar.max = Math.floor(audio.duration);
-});
-
-audio.addEventListener("timeupdate", () => {
-    progressBar.value = Math.floor(audio.currentTime);
-    currentTime.textContent = calculateTime(progressBar.value);
-});
 
 let muteState = "unmuted";
 
@@ -113,4 +100,31 @@ volume.addEventListener("click", () => {
         volume.classList = "fa-solid fa-volume-high";
         volumeBar.value = 100;
     }
+});
+
+
+const calculateTime = (totalSeconds) => {
+    const minute = Math.floor(totalSeconds / 60);
+    const seconds = Math.floor(totalSeconds % 60);
+    const updatedSeconds = seconds < 10 ? `0${seconds}`: `${seconds}`;
+    const result = `${minute}:${updatedSeconds}`;
+    return result;
+}
+
+
+audio.addEventListener("loadedmetadata", () => {
+    duration.textContent = calculateTime(audio.duration);
+    progressBar.max = Math.floor(audio.duration);
+});
+
+
+audio.addEventListener("timeupdate", () => {
+    progressBar.value = Math.floor(audio.currentTime);
+    currentTime.textContent = calculateTime(progressBar.value);
+});
+
+
+progressBar.addEventListener("input", () => {
+    currentTime.textContent = calculateTime(progressBar.value);
+    audio.currentTime = progressBar.value;
 });
